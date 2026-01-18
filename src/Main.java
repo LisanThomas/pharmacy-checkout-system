e5966ca87d5a9f4220fc7d3d03f22813bc3bf0f1
Lisan Thomas <thomas_lisan@yahoo.com>
2026-01-18 11:01:09 +1100

    Integrate Item class into Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pharmacy Checkout System");
+        ArrayList<Item> items = new ArrayList<>();
+
+        items.add(new Item("Paracetamol", 5.50));
+        items.add(new Item("Cough Syrup", 15.00));
+        items.add(new Item("Hydralite", 11.50));
+        items.add(new Item("Shampoo", 9.25));
+
+        System.out.println("Available items: ");
+
+        for (int i = 0; i < items.size(); i++) {
+            Item item = items.get(i);
+            System.out.println((i+1) + ". " + item.getName() + " - $" + item.getPrice());
+        }
     }
 }
